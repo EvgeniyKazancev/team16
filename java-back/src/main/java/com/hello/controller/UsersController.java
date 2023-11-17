626ae0414fe2afef233aab4c953956cfa7f29f3f
Evgeniy Gorkavyi <e.gorkavyi@gmail.com>
2023-11-17 06:43:41 +0600

    User and login API in progress.

--- java-back/src/main/java/com/hello/controller/UsersController.java
+++ java-back/src/main/java/com/hello/controller/UsersController.java
@@ -1,12 +1,16 @@
 package com.hello.dbservices.controller;
 
 import com.hello.dbservices.entity.Users;
-import com.hello.dbservices.entity.UsersHideSecureInfo;
+import com.hello.dbservices.entity.UsersHSI;
 import com.hello.dbservices.response.ResponseMessage;
 import com.hello.dbservices.services.UsersServices;
+import com.hello.dbservices.services.UsersServicesHSI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/users")
@@ -15,20 +19,29 @@ public class UsersController {
 
     private final UsersServices usersServices;
 
+    private final UsersServicesHSI usersServicesHSI;
+
 
     @Autowired
-    public UsersController(UsersServices usersServices) {
+    public UsersController(UsersServices usersServices, UsersServicesHSI usersServicesHSI) {
         this.usersServices = usersServices;
+        this.usersServicesHSI = usersServicesHSI;
     }
 
     @GetMapping("/getUserId")
-    public Users getUser(@RequestParam Long userId) {
-        return usersServices.getUsers(userId);
+    public UsersHSI getUser(@RequestParam Long userId, String uuid) {
+        UsersHSI usersHSI = usersServicesHSI.getUsers(userId, uuid);
+        if (usersHSI.getId() == null)
+            throw new ResponseStatusException(HttpStatusCode.valueOf(403));
+        return usersHSI;
     }
 
     @GetMapping("/getAllUsers")
-    public List<UsersHideSecureInfo> getAllUsers() {
-        return usersServices.getAllUsers();
+    public List<UsersHSI> getAllUsers(@RequestParam String uuid) {
+        List<UsersHSI> usersHSIList = usersServicesHSI.getAllUsers(uuid);
+        if (usersHSIList.isEmpty())
+            throw new ResponseStatusException(HttpStatusCode.valueOf(403));
+        return usersHSIList;
     }
 
     @PutMapping("/addUser")
